7e797851fba884e8816f75a9b32b18fea33ce318
chuongsecvay <81426435+chuongsecvay@users.noreply.github.com>
2021-04-02 22:48:03 +0700

    Update tb.v

--- Week 3/HDL/tb.v
+++ Week 3/HDL/tb.v
@@ -1,13 +1,5 @@
 
 
-
-
-
-
-
-
-
-
 module tb();
   parameter N = 32;
     
@@ -34,46 +26,38 @@ module tb();
   reg [N-1:0] binary[0:43437];
   integer out;
   integer j = 0; // pointer memory
-  
+  integer i = 0;
    
   initial begin
     $readmemb("binary.txt",binary);
+    
     #1 rst_n <= 0;
-    #1 rst_n <= 1; 
-    #8 R <= 8'd0;
-    G <= 8'd0;
-    B <= 8'd0;
-    clk <= 0;
-    i_clk <= 0;
-    i_start <= 0;
+    #1 rst_n <= 1;
+    #6 i_clk = 0;
     out = $fopen("out.txt","w");
     
-    forever #1 i_clk = !i_clk;
+    forever #2 i_clk = !i_clk;
   end
   
-  integer i = 0;
-  
-  always @(*)
+  always @(posedge i_clk)
     begin
-      if (i_clk == 1)
-        i = i + 1;
+      if (i == 47)
+        begin
+          i <= 0;
+          val_in <= 1;
+          clk <= 1;
+          i_start <= 1'b1;
+          j <= j + 3;
+          RGBinput();
+        end
       else
-        if (i == 0)
-          begin
-            RGBinput();
-            val_in <= 1;
-            clk <= 1;
-            i_start <= 1;
-            #2 val_in <= 0;
-            clk <= 0;
-            i_start <= 0;
-            j = j + 3;
-          end
-        else
-          if (i == 47)
-            i = 0;
+        begin
+          i <= i + 1;
+          val_in <= 0;
+          i_start <= 1'b0;
+          clk <= 1'b0;
+       	end
     end
-    
     
   task RGBinput();
     begin  
@@ -82,7 +66,18 @@ module tb();
       B = binary[j+2];
     end
   endtask
- 
+  
+  always @(posedge val_out)
+    begin
+      if(j>9)
+        begin
+          $fwrite(out,"%d\t",H[30:15]);
+          $fwrite(out,"%d\t",S[30:15]);
+          $fwrite(out,"%d\n",V[30:15]);
+        end
+    end
+    
+     
 endmodule
 
 
